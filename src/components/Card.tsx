3bc09424017e8beea505456ffd72897633152d0d
anri <anriugrexelidze2006@gmail.com>
2025-07-30 16:09:12 +0400

    layout fixes.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,15 +8,25 @@ type cardProps = {
 };
 
 function Card({ image, title, price, discount }: cardProps) {
+  const discountNum = discount
+    ? parseFloat(discount.replace("%", ""))
+    : 0; /* to parse string to int / remove % from it for calculations */
+  const discountedPrice =
+    price - (price * discountNum) / 100; /* discount calculation */
+
   return (
     <div className="card-main">
       <div className="card-img">
         <img src={image} alt="" />
-        <p className="imgdisc">{discount} OFF</p>
+        {discount && <p className="imgdisc">{discount} OFF</p>}
       </div>
       <div className="card-text">
         <p className="title">{title}</p>
-        <p className="price">${price}</p>
+        <div className="prices">
+          <p className="oldprice price">${discountedPrice.toFixed(2)}</p>
+          <p className="newprice price">${price.toFixed(2)}</p>
+        </div>
+        <div className="border"></div>
         {discount && <p className="discount">Save {discount}</p>}
       </div>
     </div>
