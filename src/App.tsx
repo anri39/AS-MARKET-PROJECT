668634c8f7b37b3d9c0d732f17ace905d16efb18
anri <anriugrexelidze2006@gmail.com>
2025-07-31 18:05:32 +0400

    navbar finished(needs dropdown code)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import Card from "./components/Card";
-import samsungImage from "./assets/samsung.png";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Card
-        image={samsungImage}
-        title="Galaxy S22 Ultra"
-        price={1200}
-        discount="10%"
-      />
-      <Card
-        image={samsungImage}
-        title="Galaxy S22 Ultra"
-        price={1200}
-        discount="30%"
-      />
       <Navbar />
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import { FaBars, FaSearch, FaRegUser } from "react-icons/fa";
 import { MdOutlineShoppingCart } from "react-icons/md";
+import { FiChevronDown } from "react-icons/fi";
 
 function Navbar() {
   return (
@@ -9,7 +10,7 @@ function Navbar() {
         {/* Left - Logo */}
         <div className="left-part">
           <div className="title">
-            <FaBars size={40} />
+            <FaBars size={70} />
             <span>AS-MART</span>
           </div>
         </div>
@@ -17,12 +18,12 @@ function Navbar() {
         {/* Center - Search */}
         <div className="center-part">
           <div className="search">
-            <FaSearch size={20} />
+            <FaSearch size={30} />
             <input
               type="text"
               placeholder="Search essentials, groceries and more..."
             />
-            <FaBars size={24} />
+            <FaBars size={30} />
           </div>
         </div>
 
@@ -32,10 +33,56 @@ function Navbar() {
             <FaRegUser size={30} />
             <span>Sign Up/Sign In</span>
           </div>
+          <p className="nav-border"></p>
           <div className="cart">
-            <MdOutlineShoppingCart size={30} />
+            <MdOutlineShoppingCart size={40} />
             <span>Cart</span>
           </div>
+        </div>
+      </div>
+
+      {/* Bottom nav with dropdown */}
+      <div className="bottom-part">
+        <div className="nav-item-with-dropdown">
+          <div className="nav-item">
+            <span>Groceries</span>
+            <FiChevronDown size={20} className="dropdown-icon" />
+          </div>
+        </div>
+
+        <div className="nav-item">
+          <span>Premium Fruits</span>
+          <FiChevronDown size={20} className="dropdown-icon" />
+        </div>
+
+        <div className="nav-item">
+          <span>Home & Kitchen</span>
+          <FiChevronDown size={20} className="dropdown-icon" />
+        </div>
+
+        <div className="nav-item">
+          <span>Fashion</span>
+          <FiChevronDown size={20} className="dropdown-icon" />
+        </div>
+
+        <div className="nav-item">
+          <span>Electronics</span>
+          <FiChevronDown size={20} className="dropdown-icon" />
+        </div>
+
+        <div className="nav-item">
+          <span>Beauty</span>
+          <FiChevronDown size={20} className="dropdown-icon" />
+        </div>
+
+        <div className="nav-item">
+          <span>Home Improvement</span>
+          <FiChevronDown size={20} className="dropdown-icon" />
+        </div>
+
+        <div className="nav-item">
+          <span>Sports, Toys & Luggage</span>
+          <FiChevronDown size={20} className="dropdown-icon" />
         </div>
       </div>
     </div>
