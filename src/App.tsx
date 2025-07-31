dc3b1f61df889bb1e423dcd3fe7600388fcda672
DrupiFan <sabakublashvili505@gmail.com>
2025-07-31 20:23:30 +0400

    Added a slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import CustomSlider from "./components/Slider";
 
 function App() {
   return (
     <>
       <Navbar />
+
+      <CustomSlider />
 
       <Footer />
     </>
---
+++ src/components/Slider.tsx
@@ -0,0 +1,34 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Slider.css";
+import React from "react";
+import Slider from "react-slick";
+
+const CustomSlider: React.FC = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  return (
+    <div style={{ width: "80%", margin: "0 auto", paddingTop: "50px" }}>
+      <Slider {...settings}>
+        <div>
+          <img src="" alt="Slide 1" className="slider-image" />
+        </div>
+        <div>
+          <img src="" alt="Slide 2" className="slider-image" />
+        </div>
+      </Slider>
+
+    </div>
+  );
+};
+
+export default CustomSlider;
