f515aba2dd5e485cbb0ba1acfd4c10954c4047fe
anri <anriugrexelidze2006@gmail.com>
2025-08-04 18:29:11 +0400

    Navbar and Slider bug fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,83 +6,66 @@ import { FiChevronDown } from "react-icons/fi";
 function Navbar() {
   return (
     <div className="navbar-main">
-      <div className="top-part">
-        {/* Left - Logo */}
-        <div className="left-part">
-          <div className="title">
-            <FaBars size={70} />
-            <span>AS-MART</span>
+      <div className="page-container">
+        {/* Top Section */}
+        <div className="top-part">
+          {/* Left - Logo */}
+          <div className="left-part">
+            <div className="title">
+              <FaBars size={70} />
+              <span>AS-MART</span>
+            </div>
+          </div>
+
+          {/* Center - Search */}
+          <div className="center-part">
+            <div className="search">
+              <FaSearch size={30} />
+              <input
+                type="text"
+                placeholder="Search essentials, groceries and more..."
+              />
+              <FaBars size={30} />
+            </div>
+          </div>
+
+          {/* Right - Sign In and Cart */}
+          <div className="right-part">
+            <div className="sign">
+              <FaRegUser size={30} />
+              <span>Sign Up/Sign In</span>
+            </div>
+            <p className="nav-border"></p>
+            <div className="cart">
+              <MdOutlineShoppingCart size={40} />
+              <span>Cart</span>
+            </div>
           </div>
         </div>
 
-        {/* Center - Search */}
-        <div className="center-part">
-          <div className="search">
-            <FaSearch size={30} />
-            <input
-              type="text"
-              placeholder="Search essentials, groceries and more..."
-            />
-            <FaBars size={30} />
+        {/* Bottom Navigation */}
+        <div className="bottom-part">
+          <div className="nav-item-with-dropdown">
+            <div className="nav-item">
+              <span>Groceries</span>
+              <FiChevronDown size={20} className="dropdown-icon" />
+            </div>
           </div>
-        </div>
 
-        {/* Right - Sign and Cart */}
-        <div className="right-part">
-          <div className="sign">
-            <FaRegUser size={30} />
-            <span>Sign Up/Sign In</span>
-          </div>
-          <p className="nav-border"></p>
-          <div className="cart">
-            <MdOutlineShoppingCart size={40} />
-            <span>Cart</span>
-          </div>
-        </div>
-      </div>
-
-      {/* Bottom nav with dropdown */}
-      <div className="bottom-part">
-        <div className="nav-item-with-dropdown">
-          <div className="nav-item">
-            <span>Groceries</span>
-            <FiChevronDown size={20} className="dropdown-icon" />
-          </div>
-        </div>
-
-        <div className="nav-item">
-          <span>Premium Fruits</span>
-          <FiChevronDown size={20} className="dropdown-icon" />
-        </div>
-
-        <div className="nav-item">
-          <span>Home & Kitchen</span>
-          <FiChevronDown size={20} className="dropdown-icon" />
-        </div>
-
-        <div className="nav-item">
-          <span>Fashion</span>
-          <FiChevronDown size={20} className="dropdown-icon" />
-        </div>
-
-        <div className="nav-item">
-          <span>Electronics</span>
-          <FiChevronDown size={20} className="dropdown-icon" />
-        </div>
-
-        <div className="nav-item">
-          <span>Beauty</span>
-          <FiChevronDown size={20} className="dropdown-icon" />
-        </div>
-
-        <div className="nav-item">
-          <span>Home Improvement</span>
-          <FiChevronDown size={20} className="dropdown-icon" />
-        </div>
-
-        <div className="nav-item">
-          <span>Sports, Toys & Luggage</span>
-          <FiChevronDown size={20} className="dropdown-icon" />
+          {[
+            "Premium Fruits",
+            "Home & Kitchen",
+            "Fashion",
+            "Electronics",
+            "Beauty",
+            "Home Improvement",
+            "Sports, Toys & Luggage",
+          ].map((item) => (
+            <div className="nav-item" key={item}>
+              <span>{item}</span>
+              <FiChevronDown size={20} className="dropdown-icon" />
+            </div>
+          ))}
         </div>
       </div>
     </div>
