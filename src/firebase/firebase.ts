b1673c2b80f4c474f809a5aa6c420a380d702150
Anri Ugrekhelidze <anriugrexelidze2006@gmail.com>
2025-08-21 22:12:18 +0400

    minor fix

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth"; // Import Authentication (if using)
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDfHouI-UZ0qYjW_9tpFa-gGfV9kExzWKc",
-  authDomain: "as-market-78929.firebaseapp.com",
-  projectId: "as-market-78929",
-  storageBucket: "as-market-78929.firebasestorage.app",
-  messagingSenderId: "1093786490282",
-  appId: "1:1093786490282:web:7c0aaad4c385f8508147d3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
